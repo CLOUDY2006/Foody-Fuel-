b487c35a7fa1c82b1af2bff017ef7fb47c29aa1c
Arnav Kumar Gupta <arnavg0106@gmail.com>
2025-10-04 23:00:22 +0530

    Update vite.config.ts: set base to '/' and outDir to 'dist' for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/CLOUDY2006/',  
+    base: '/',  
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -52,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
